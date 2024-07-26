568942c6b4cb856af220252d94d451cc0131751c
wayne0419 <44443025+wayne0419@users.noreply.github.com>
2024-07-27 02:07:49 +0800

    group body parts into body display controller

--- Assets/BodyDisplayController.cs
+++ Assets/BodyDisplayController.cs
@@ -33,6 +33,29 @@ public class BodyDisplayController : MonoBehaviour
     {
         foreach (Material mat in renderer.materials)
         {
+            if (alpha < 1.0f)
+            {
+                mat.SetFloat("_Mode", 2); // Transparent mode
+                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                mat.SetInt("_ZWrite", 0);
+                mat.DisableKeyword("_ALPHATEST_ON");
+                mat.EnableKeyword("_ALPHABLEND_ON");
+                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                mat.renderQueue = 3000;
+            }
+            else
+            {
+                mat.SetFloat("_Mode", 0); // Opaque mode
+                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                mat.SetInt("_ZWrite", 1);
+                mat.DisableKeyword("_ALPHATEST_ON");
+                mat.DisableKeyword("_ALPHABLEND_ON");
+                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                mat.renderQueue = -1;
+            }
+
             Color color = mat.color;
             color.a = alpha;
             mat.color = color;
