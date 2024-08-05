e1aaa0ddde8e8725c128d14e7358a5650548adc2
tk <44443025+wayne0419@users.noreply.github.com>
2024-08-05 15:24:02 +0800

    change review drum sheet note color, add two practice sample

--- Assets/ReviewSheetDrawer.cs
+++ Assets/ReviewSheetDrawer.cs
@@ -12,15 +12,8 @@ public class ReviewSheetDrawer : MonoBehaviour
     public GameObject pointPrefab; // 预制体用于打击点
 
     // 用于视觉化的颜色
-    public Color snareDrumColor = Color.red;
-    public Color bassDrumColor = Color.blue;
-    public Color closedHiHatColor = Color.green;
-    public Color tom1Color = Color.yellow;
-    public Color tom2Color = Color.magenta;
-    public Color floorTomColor = Color.cyan;
-    public Color crashColor = Color.grey;
-    public Color rideColor = Color.white;
-    public Color openHiHatColor = Color.black;
+    public Color userColor = Color.blue; // 用户打击点的颜色
+    public Color targetColor = Color.red; // 目标打击点的颜色
 
     private List<GameObject> points = new List<GameObject>();
 
@@ -32,26 +25,27 @@ public class ReviewSheetDrawer : MonoBehaviour
             return;
         }
 
-        // 绘制每种鼓的打击点
-        DrawDrumHits(reviewManager.userSnareDrumHits, snareDrumColor, 0);
-        DrawDrumHits(reviewManager.userBassDrumHits, bassDrumColor, 1);
-        DrawDrumHits(reviewManager.userClosedHiHatHits, closedHiHatColor, 2);
-        DrawDrumHits(reviewManager.userTom1Hits, tom1Color, 3);
-        DrawDrumHits(reviewManager.userTom2Hits, tom2Color, 4);
-        DrawDrumHits(reviewManager.userFloorTomHits, floorTomColor, 5);
-        DrawDrumHits(reviewManager.userCrashHits, crashColor, 6);
-        DrawDrumHits(reviewManager.userRideHits, rideColor, 7);
-        DrawDrumHits(reviewManager.userOpenHiHatHits, openHiHatColor, 8);
+        // 绘制用户打击点
+        DrawDrumHits(reviewManager.userSnareDrumHits, userColor, 0);
+        DrawDrumHits(reviewManager.userBassDrumHits, userColor, 1);
+        DrawDrumHits(reviewManager.userClosedHiHatHits, userColor, 2);
+        DrawDrumHits(reviewManager.userTom1Hits, userColor, 3);
+        DrawDrumHits(reviewManager.userTom2Hits, userColor, 4);
+        DrawDrumHits(reviewManager.userFloorTomHits, userColor, 5);
+        DrawDrumHits(reviewManager.userCrashHits, userColor, 6);
+        DrawDrumHits(reviewManager.userRideHits, userColor, 7);
+        DrawDrumHits(reviewManager.userOpenHiHatHits, userColor, 8);
 
-        DrawDrumHits(reviewManager.targetSnareDrumHits, snareDrumColor, 0.5f);
-        DrawDrumHits(reviewManager.targetBassDrumHits, bassDrumColor, 1.5f);
-        DrawDrumHits(reviewManager.targetClosedHiHatHits, closedHiHatColor, 2.5f);
-        DrawDrumHits(reviewManager.targetTom1Hits, tom1Color, 3.5f);
-        DrawDrumHits(reviewManager.targetTom2Hits, tom2Color, 4.5f);
-        DrawDrumHits(reviewManager.targetFloorTomHits, floorTomColor, 5.5f);
-        DrawDrumHits(reviewManager.targetCrashHits, crashColor, 6.5f);
-        DrawDrumHits(reviewManager.targetRideHits, rideColor, 7.5f);
-        DrawDrumHits(reviewManager.targetOpenHiHatHits, openHiHatColor, 8.5f);
+        // 绘制目标打击点
+        DrawDrumHits(reviewManager.targetSnareDrumHits, targetColor, 0.5f);
+        DrawDrumHits(reviewManager.targetBassDrumHits, targetColor, 1.5f);
+        DrawDrumHits(reviewManager.targetClosedHiHatHits, targetColor, 2.5f);
+        DrawDrumHits(reviewManager.targetTom1Hits, targetColor, 3.5f);
+        DrawDrumHits(reviewManager.targetTom2Hits, targetColor, 4.5f);
+        DrawDrumHits(reviewManager.targetFloorTomHits, targetColor, 5.5f);
+        DrawDrumHits(reviewManager.targetCrashHits, targetColor, 6.5f);
+        DrawDrumHits(reviewManager.targetRideHits, targetColor, 7.5f);
+        DrawDrumHits(reviewManager.targetOpenHiHatHits, targetColor, 8.5f);
     }
 
     private void DrawDrumHits(List<ReviewManager.TransformDataIndex> drumHits, Color color, float yIndex)
@@ -59,8 +53,9 @@ public class ReviewSheetDrawer : MonoBehaviour
         foreach (var hit in drumHits)
         {
             Vector3 position = new Vector3(hit.data.timestamp * timeScale, yOffset * yIndex, 0);
-            GameObject point = Instantiate(pointPrefab, position, Quaternion.identity);
+            GameObject point = Instantiate(pointPrefab, position, Quaternion.identity, this.transform);
             point.GetComponent<Renderer>().material.color = color;
+            point.transform.localScale = new Vector3(pointSize, pointSize, pointSize);
             points.Add(point);
         }
     }
