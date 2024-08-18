cfb11a466a23ddbc39db3720c3ad13302b8008d6
wayne0419 <44443025+wayne0419@users.noreply.github.com>
2024-08-19 06:54:08 +0800

    add drum note, drum sheet, drum type

---
+++ Assets/DrumNote.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class DrumNote : MonoBehaviour
+{
+    public DrumType drumType; // 鼓的类型
+    public Color skippedColor; // 跳过时的颜色
+    public Color defaultColor; // 默认颜色
+    public Color highlightColor; // 高亮时的颜色
+
+    private Renderer noteRenderer;
+
+    void Awake()
+    {
+        noteRenderer = GetComponent<Renderer>();
+        if (noteRenderer != null)
+        {
+            noteRenderer.material.color = defaultColor; // 设置为默认颜色
+        }
+    }
+
+    public void SetSkippedColor()
+    {
+        if (noteRenderer != null)
+        {
+            noteRenderer.material.color = skippedColor;
+        }
+    }
+
+    public void SetDefaultColor()
+    {
+        if (noteRenderer != null)
+        {
+            noteRenderer.material.color = defaultColor;
+        }
+    }
+
+    public void SetHighlightColor()
+    {
+        if (noteRenderer != null)
+        {
+            noteRenderer.material.color = highlightColor;
+        }
+    }
+}
---
+++ Assets/DrumType.cs
@@ -0,0 +1,12 @@
+public enum DrumType
+{
+    BassDrum,
+    SnareDrum,
+    ClosedHiHat,
+    Tom1,
+    Tom2,
+    FloorTom,
+    Crash,
+    Ride,
+    OpenHiHat
+}
