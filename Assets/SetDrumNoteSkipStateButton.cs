06057825c0ed172e640420a747e047680b3f2634
tk <44443025+wayne0419@users.noreply.github.com>
2024-08-27 18:51:43 +0800

    add buttons for level button switching

--- Assets/SetDrumNoteSkipStateButton.cs
+++ Assets/SetDrumNoteSkipStateButton.cs
@@ -50,6 +50,7 @@ public class SetDrumNoteSkipStateButton : MonoBehaviour
     }
     void Update() {
         if (inputAction.triggered) {
+            Debug.Log("SetDrumNoteSkipState");
             OnMouseDown();
         }
     }
--- Assets/SetHitDrumCorrectMode.cs
+++ Assets/SetHitDrumCorrectMode.cs
@@ -16,6 +16,7 @@ public class SetHitDrumCorrectMode : MonoBehaviour
     }
     void Update() {
         if (inputAction.triggered) {
+            Debug.Log("SetHitDrumCorrectMode");
             OnMouseDown();
         }
     }
